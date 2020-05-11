2f1367a8affd217862198c15f75e13050e4a63e3
Julie Park <jspark721@gmail.com>
2020-05-11 14:43:02 -0700

    bubble up the state to the parent App

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,8 +5,11 @@ class NavBar extends Component {
   render() {
     return (
       <nav className='navbar navbar-light bg-light'>
-        <a className='navbar-brand' href='#'>
-          Navbar
+        <a className='navbar-brand' href='/'>
+          Navbar{' '}
+          <span className='badge badge-pill badge-secondary'>
+            {this.props.totalCounters}
+          </span>
         </a>
       </nav>
     );
