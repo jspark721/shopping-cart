36f401b6180c0f10dda7cf5b5bc038c9dd04cbb7
Julie Park <jspark721@gmail.com>
2020-05-11 13:07:27 -0700

    event handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -9,9 +9,11 @@ class Counter extends Component {
   state = {
     count: 0,
     tags: ['tag1', 'tag2', 'tag3'],
+    product: 'shoes',
   };
 
-  handleIncrement = () => {
+  handleIncrement = (product) => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
   };
   render() {
@@ -19,7 +21,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement(product)}
           className='btn btn-secondary btn-sm'
         >
           Increment
