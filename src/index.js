4e11738d3d247544475c78d3cf9d4c96533c002f
Julie Park <jspark721@gmail.com>
2020-05-11 14:30:44 -0700

    add navbar and render App instead of counter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counters from './components/counters';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counters />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
